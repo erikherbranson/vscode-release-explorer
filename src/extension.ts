1adbfae186002e7e68045362e53c2fa4a74e8ad7
Erik Herbranson <56943367+erikherbranson@users.noreply.github.com>
2022-03-19 16:40:23 -0400

    feat(releases view): list dependencies and fetch rrs feeds for the most recent releases

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,41 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {}
+import { PackageReleasesProvider } from "../vscode-release-explorer-core/src/PackageReleasesProvider";
+import { RssBot } from "../vscode-release-explorer-core/src/RssBot";
 
-export function deactivate() {}
+let rssBot: RssBot | null;
+
+export function activate(context: vscode.ExtensionContext) {
+  const rootPath =
+    vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : undefined;
+
+  const provider = new PackageReleasesProvider(rootPath, context);
+
+  rssBot = new RssBot([provider], context);
+  rssBot.startAll();
+
+  const treeDataProviderDisposable = vscode.window.registerTreeDataProvider(
+    "releaseExplorer.views.dependencies",
+    provider
+  );
+  context.subscriptions.push(treeDataProviderDisposable);
+
+  const openLinkDisposable = vscode.commands.registerCommand(
+    "releaseExplorer.commands.openLink",
+    (link: string) => vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(link))
+  );
+  context.subscriptions.push(openLinkDisposable);
+
+  const syncSpinDisposable = vscode.commands.registerCommand(
+    "releaseExplorer.commands.syncSpin",
+    () => {}
+  );
+  context.subscriptions.push(syncSpinDisposable);
+}
+
+export function deactivate() {
+  rssBot?.stopAll();
+  rssBot = null;
+}
